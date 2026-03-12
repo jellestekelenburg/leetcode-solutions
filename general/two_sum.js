9eb8a72796fa53e2e88ddf83da3dd4e90b428d1a
jellestekelenburg <jellemaarten02@gmail.com>
2026-03-12 15:03:18 +0100

    add twoSum function to find indices of two numbers that add up to target

---
+++ general/two_sum.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+let twoSum = function(nums, target) {
+    for(let a = 0; a<nums.length; a++) {
+        for (let b = a + 1; b < nums.length; b++) {
+            if (nums[a] + nums[b] === target) {
+                return [a, b]
+            }
+        }
+    }
+}
+
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
+console.log(twoSum([2,4,8],8))
+
+/*
+1. Two Sum
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ */
