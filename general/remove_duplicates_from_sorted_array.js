83f5e2482a0036ce96693ad04398c5577f543067
jellestekelenburg <jellemaarten02@gmail.com>
2026-07-09 20:56:06 +0200

    Implement removeDuplicates function to eliminate duplicates from a sorted array

---
+++ general/remove_duplicates_from_sorted_array.js
@@ -0,0 +1,84 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Two pointer solution
+var removeDuplicates = function(nums) {
+    if (nums.length < 1) return nums
+
+    let w = 0;
+
+    for (let r = 1; r < nums.length; r++) {
+        if(nums[w] !== nums[r]) {
+            w++;
+            nums[w] = nums[r];
+        }
+    }
+
+    return w + 1;
+};
+
+// Array change (slower)
+var removeDuplicates_ = function(nums) {
+    let dup = 0;
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] === nums[i + 1]) {
+            if (i > -1) {
+                nums.splice(i, 1);
+                dup++;
+            }
+        }
+    }
+
+    let k = nums.length;
+    for (let i = 0; i < dup; i++) {
+        nums.push('_')
+    }
+
+    return k
+    
+};
+
+console.log(removeDuplicates([1,2,2,2,3,3,3,3,3,5]))
+
+
+
+/**
+ * 26. Remove Duplicates from Sorted Array
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+ */
